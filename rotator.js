05df94692b8a032bbd79fa83fb4fee663ac0d084
brasilisnet <83701053+brasilisnet@users.noreply.github.com>
2021-05-05 23:48:51 +0900

    Add files via upload

---
+++ rotator.js
@@ -0,0 +1,9 @@
+mybanners=
+[
+"<a href=\"https://www.erosmania.com.br/produtos.asp?cod=070726&parc=2044\" target=\"_blank\"><img src=\"https://1.bp.blogspot.com/-w38B-D408P0/YJKfSNb-vrI/AAAAAAAAt1Y/bAAZovARZcEgT5sbbmg6p-FW5ltjeTEcgCPcBGAYYCw/s0/ku-loko.jpg\" width=\"500\" height=\"200\" alt=\"Erosmania\" title=\"Erosmania\" border=\"0\"></a>",
+"<a href=\"https://www.erosmania.com.br/produtos.asp?cod=070730&parc=2044\" target=\"_blank\"><img src=\"https://1.bp.blogspot.com/-XB0XzERIQLE/YJKfSGBA_cI/AAAAAAAAt1g/KeCWRLWj_Z8ESX-aU-LaLPWpG86Ar8nCwCPcBGAYYCw/s0/excit-analis.jpg\" width=\"500\" height=\"200\" alt=\"Erosmania\" title=\"Erosmania\" border=\"0\"></a>",
+"<a href=\"https://www.erosmania.com.br/produtos.asp?cod=073231&parc=2044\" target=\"_blank\"><img src=\"https://1.bp.blogspot.com/-grX5FrQ0R94/YJKfSJnqfGI/AAAAAAAAt1c/GS_xL1pQmss_3nSO6OqN6gCpcFK18JwPgCPcBGAYYCw/s0/xana-maravilha.jpg\" width=\"500\" height=\"200\" alt=\"Erosmania\" title=\"Erosmania\" border=\"0\"></a>"
+]
+randomNumber = Math.random()
+var show_mybanners = mybanners[Math.floor(randomNumber * mybanners.length)]
+document.write(show_mybanners);
