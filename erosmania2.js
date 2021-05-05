f87503e92ed361009f6af06038808d91f2387577
brasilisnet <83701053+brasilisnet@users.noreply.github.com>
2021-05-05 22:28:12 +0900

    Add files via upload

--- erosmania2.js
+++ erosmania2.js
@@ -1,7 +1,7 @@
 banner1 = new Banner('banner1');
-                    banner1.add("IMAGE", "https://images2.imgbox.com/13/2f/tQuBHy1Z_o.jpg", 5, 200, 500,"https://www.erosmania.com.br/produtos.asp?cod=073231&parc=2044");
-                    banner1.add("IMAGE", "https://images2.imgbox.com/37/cf/bVzUsYwy_o.jpg", 5, 202, 502,"https://www.erosmania.com.br/produtos.asp?cod=070730&parc=2044");
-                    banner1.add("IMAGE", "https://images2.imgbox.com/2d/41/FWnUG1jO_o.jpg", 5, 200, 500,"https://www.erosmania.com.br/produtos.asp?cod=070726&parc=2044");
+                    banner1.add('IMAGE', 'https://images2.imgbox.com/07/09/efewqJjK_o.jpg', 10, 200, 500,'https://www.erosmania.com.br/produtos.asp?cod=073231&parc=2044');
+                    banner1.add('IMAGE', 'https://images2.imgbox.com/6e/3f/pmBWzRzb_o.jpg', 10, 202, 502,'https://www.erosmania.com.br/produtos.asp?cod=070730&parc=2044');
+                    banner1.add('IMAGE', 'https://images2.imgbox.com/fc/e4/JetTlrfx_o.jpg', 10, 200, 500,'https://www.erosmania.com.br/produtos.asp?cod=070726&parc=2044');
                   
                     document.write(banner1);
                     banner1.start();
