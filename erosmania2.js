7a4d32d39d32008723a02448a40b5ebbcf02e859
brasilisnet <83701053+brasilisnet@users.noreply.github.com>
2021-05-05 22:40:16 +0900

    Add files via upload

--- erosmania2.js
+++ erosmania2.js
@@ -1,7 +1,7 @@
 banner1 = new Banner('banner1');
-                    banner1.add('IMAGE', 'https://images2.imgbox.com/07/09/efewqJjK_o.jpg', 10, 200, 500,'https://www.erosmania.com.br/produtos.asp?cod=073231&parc=2044');
-                    banner1.add('IMAGE', 'https://images2.imgbox.com/6e/3f/pmBWzRzb_o.jpg', 10, 202, 502,'https://www.erosmania.com.br/produtos.asp?cod=070730&parc=2044');
-                    banner1.add('IMAGE', 'https://images2.imgbox.com/fc/e4/JetTlrfx_o.jpg', 10, 200, 500,'https://www.erosmania.com.br/produtos.asp?cod=070726&parc=2044');
+                    banner1.add('IMAGE', 'https://1.bp.blogspot.com/-grX5FrQ0R94/YJKfSJnqfGI/AAAAAAAAt1M/d7E0EbPJUmop5jZW2ODWiLX90WRZzRDNQCLcBGAsYHQ/s0/xana-maravilha.jpg', 10, 200, 500,'https://www.erosmania.com.br/produtos.asp?cod=073231&parc=2044');
+                    banner1.add('IMAGE', 'https://1.bp.blogspot.com/-XB0XzERIQLE/YJKfSGBA_cI/AAAAAAAAt1U/WJQFuPLrshoya3pOMrHgEULTxd1Gmj-dwCLcBGAsYHQ/s0/excit-analis.jpg', 10, 202, 502,'https://www.erosmania.com.br/produtos.asp?cod=070730&parc=2044');
+                    banner1.add('IMAGE', 'https://1.bp.blogspot.com/-w38B-D408P0/YJKfSNb-vrI/AAAAAAAAt1Q/ZJ7lJp3NkC4hKGlveZ6DnBA8LykG68OGQCLcBGAsYHQ/s0/ku-loko.jpg', 10, 200, 500,'https://www.erosmania.com.br/produtos.asp?cod=070726&parc=2044');
                   
                     document.write(banner1);
                     banner1.start();
