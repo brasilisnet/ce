c7561d27861eb7e9ccecdfb0f56404d8c0dd1438
brasilisnet <83701053+brasilisnet@users.noreply.github.com>
2021-08-15 23:07:38 +0900

    Update rotator.js

--- rotator.js
+++ rotator.js
@@ -1,8 +1,8 @@
 mybanners=
 [
-"<a href=\"https://www.erosmania.com.br/produtos.asp?cod=070726&parc=2044\" target=\"_blank\"><img src=\"https://images2.imgbox.com/fc/e4/JetTlrfx_o.jpg\" width=\"500\" height=\"200\" alt=\"Erosmania\" title=\"Erosmania\" border=\"0\"></a>",
-"<a href=\"https://www.erosmania.com.br/produtos.asp?cod=070730&parc=2044\" target=\"_blank\"><img src=\"https://images2.imgbox.com/6e/3f/pmBWzRzb_o.jpg\" width=\"500\" height=\"200\" alt=\"Erosmania\" title=\"Erosmania\" border=\"0\"></a>",
-"<a href=\"https://www.erosmania.com.br/produtos.asp?cod=073231&parc=2044\" target=\"_blank\"><img src=\"https://images2.imgbox.com/07/09/efewqJjK_o.jpg\" width=\"500\" height=\"200\" alt=\"Erosmania\" title=\"Erosmania\" border=\"0\"></a>"
+"<a href=\"https://www.erosmania.com.br/produtos.asp?cod=070726&parc=2044\" target=\"_blank\"><img src=\"https://images2.imgbox.com/ce/cb/3SN9chPz_o.jpg\" width=\"500\" height=\"200\" alt=\"Erosmania\" title=\"Erosmania\" border=\"0\"></a>",
+"<a href=\"https://www.erosmania.com.br/produtos.asp?cod=070730&parc=2044\" target=\"_blank\"><img src=\"https://images2.imgbox.com/47/40/A6iiwxCb_o.jpg\" width=\"500\" height=\"200\" alt=\"Erosmania\" title=\"Erosmania\" border=\"0\"></a>",
+"<a href=\"https://www.erosmania.com.br/produtos.asp?cod=073231&parc=2044\" target=\"_blank\"><img src=\"https://images2.imgbox.com/c0/2d/6CyxhUmb_o.jpg\" width=\"500\" height=\"200\" alt=\"Erosmania\" title=\"Erosmania\" border=\"0\"></a>"
 ]
 randomNumber = Math.random()
 var show_mybanners = mybanners[Math.floor(randomNumber * mybanners.length)]
