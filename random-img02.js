c5046089d43a0acbe6db3130e8a8ce679aa9cb0f
brasilisnet <83701053+brasilisnet@users.noreply.github.com>
2021-08-17 21:56:42 +0900

    Update random-img02.js

--- random-img02.js
+++ random-img02.js
@@ -1,4 +1,4 @@
-function random_imglink(){
+function random_imglink02(){
 var myimages02=new Array()
 //specify random images below. You can have as many as you wish
 myimages02[1]="https://images2.imgbox.com/92/17/pJAc2FYI_o.jpg"
@@ -8,6 +8,6 @@ myimages03[3]="https://images2.imgbox.com/be/10/WRKXuKjh_o.jpg"
 var ry=Math.floor(Math.random()*myimages02.length)
 if (ry==0)
 ry=1
-document.write('<img src="'+myimages02[ry]+'" border=0 width=100% height=auto>')
+document.write('<img src="'+myimages02[ry]+'" border=0>')
 }
-random_imglink()
+random_imglink02()
