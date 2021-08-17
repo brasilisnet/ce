17aa49101061a74a8545b4c7e059e260778d6e75
brasilisnet <83701053+brasilisnet@users.noreply.github.com>
2021-08-17 21:46:13 +0900

    Update random-img01.js

--- random-img01.js
+++ random-img01.js
@@ -1,4 +1,4 @@
-function random_imglink01(){
+function random_imglink(){
 var myimages01=new Array()
 //specify random images below. You can have as many as you wish
 myimages01[1]="https://images2.imgbox.com/c1/01/5oRJdaF4_o.jpg"
@@ -8,6 +8,6 @@ myimages01[3]="https://images2.imgbox.com/67/eb/KpEHLoGh_o.jpg"
 var ry=Math.floor(Math.random()*myimages01.length)
 if (ry==0)
 ry=1
-document.write('<img src="'+myimages01[ry]+'" border=0 width="100%" height="auto">')
+document.write('<img src="'+myimages01[ry]+'" border=0 width=100% height=auto>')
 }
-random_imglink01()
+random_imglink()
