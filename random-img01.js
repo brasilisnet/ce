b2d1e70375677b8205a798646fdaceb607df3dcf
brasilisnet <83701053+brasilisnet@users.noreply.github.com>
2021-08-17 21:56:06 +0900

    Update random-img01.js

--- random-img01.js
+++ random-img01.js
@@ -1,13 +1,13 @@
-function random_imglink(){
+function random_imglink01(){
 var myimages01=new Array()
 //specify random images below. You can have as many as you wish
-myimages01[1]="https://images2.imgbox.com/c1/01/5oRJdaF4_o.jpg"
-myimages01[2]="https://images2.imgbox.com/43/b6/BmDsUmwx_o.jpg"
-myimages01[3]="https://images2.imgbox.com/67/eb/KpEHLoGh_o.jpg"
+myimages[1]="https://images2.imgbox.com/c1/01/5oRJdaF4_o.jpg"
+myimages[2]="https://images2.imgbox.com/43/b6/BmDsUmwx_o.jpg"
+myimages[3]="https://images2.imgbox.com/67/eb/KpEHLoGh_o.jpg"
 
-var ry=Math.floor(Math.random()*myimages01.length)
+var ry=Math.floor(Math.random()*myimages.length)
 if (ry==0)
 ry=1
-document.write('<img src="'+myimages01[ry]+'" border=0 width=100% height=auto>')
+document.write('<img src="'+myimages01[ry]+'" border=0>')
 }
-random_imglink()
+random_imglink01()
