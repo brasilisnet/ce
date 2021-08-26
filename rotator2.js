d6bdb3d741ae33550374f8292d9c89b14c9b2f2d
brasilisnet <83701053+brasilisnet@users.noreply.github.com>
2021-08-26 19:44:33 +0900

    Update rotator2.js

--- rotator2.js
+++ rotator2.js
@@ -2,7 +2,7 @@ mybanners2=
 [
 "<a href=\"https://www.erosmania.com.br/produtos.asp?cod=070707&parc=2044\" target=\"_blank\"><img src=\"https://images2.imgbox.com/cb/bd/GQlxhUue_o.jpg\" width=\"500\" height=\"200\" alt=\"Erosmania\" title=\"Erosmania\" border=\"0\"></a>",
 "<a href=\"https://www.erosmania.com.br/produtos.asp?cod=071120&parc=2044\" target=\"_blank\"><img src=\"https://images2.imgbox.com/e9/ab/EraF2wJ1_o.jpg\" width=\"500\" height=\"200\" alt=\"Erosmania\" title=\"Erosmania\" border=\"0\"></a>",
-"<a href=\"https://www.erosmania.com.br/produtos.asp?cod=021628&parc=2044\" target=\"_blank\"><img src=\"hhttps://images2.imgbox.com/53/f6/0V0YidWX_o.jpg\" width=\"500\" height=\"200\" alt=\"Erosmania\" title=\"Erosmania\" border=\"0\"></a>"
+"<a href=\"https://www.erosmania.com.br/produtos.asp?cod=021628&parc=2044\" target=\"_blank\"><img src=\"https://images2.imgbox.com/53/f6/0V0YidWX_o.jpg\" width=\"500\" height=\"200\" alt=\"Erosmania\" title=\"Erosmania\" border=\"0\"></a>"
 ]
 randomNumber = Math.random()
 var show_mybanners2 = mybanners2[Math.floor(randomNumber * mybanners2.length)]
