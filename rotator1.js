07b8bca86002e44ed565493c6b3f51a3f29cc192
brasilisnet <83701053+brasilisnet@users.noreply.github.com>
2024-01-01 11:34:04 +0900

    Delete rotator1.js

--- rotator1.js
+++
@@ -1,9 +0,0 @@
-mybanners1=
-[
-"<a href=\"https://www.erosmania.com.br/produtos.asp?cod=070202&parc=2044\" target=\"_blank\"><img src=\"https://images2.imgbox.com/c0/96/RqmuvlYj_o.jpg\" width=\"500\" height=\"200\" alt=\"Erosmania\" title=\"Erosmania\" border=\"0\"></a>",
-"<a href=\"https://www.erosmania.com.br/produtos.asp?cod=070808&parc=2044\" target=\"_blank\"><img src=\"https://images2.imgbox.com/38/f6/PSbclkti_o.jpg\" width=\"500\" height=\"200\" alt=\"Erosmania\" title=\"Erosmania\" border=\"0\"></a>",
-"<a href=\"https://www.erosmania.com.br/produtos.asp?cod=070602&parc=2044\" target=\"_blank\"><img src=\"https://images2.imgbox.com/ba/4e/mzDJBZSj_o.jpg\" width=\"500\" height=\"200\" alt=\"Erosmania\" title=\"Erosmania\" border=\"0\"></a>"
-]
-randomNumber = Math.random()
-var show_mybanners1 = mybanners1[Math.floor(randomNumber * mybanners1.length)]
-document.write(show_mybanners1);
